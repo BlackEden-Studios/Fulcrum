a537b85133672a0a96c2229917a2b4c9affff6a6
Nico Visci <n.visci22@gmail.com>
2025-10-14 23:31:06 +0200

    First commit

---
+++ fulcrum-plugin/src/main/java/com/bestudios/fulcrum/Fulcrum.java
@@ -0,0 +1,17 @@
+package com.bestudios.fulcrum;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Fulcrum extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
