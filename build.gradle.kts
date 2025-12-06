af4d237ffd2571c3984b120b7c776df5383a635d
Nico Visci <n.visci22@gmail.com>
2025-12-06 23:50:38 +0100

    Adding DummySaver

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "io.github.blackeden-studios"
-    version = "0.1.4-BETA"
+    version = "0.1.5-BETA"
 
     repositories {
         mavenCentral()
---
+++ fulcrum-api/src/main/java/com/bestudios/fulcrum/api/util/DummySaver.java
@@ -0,0 +1,19 @@
+package com.bestudios.fulcrum.api.util;
+
+import com.bestudios.fulcrum.api.cache.PlayerDataSaver;
+
+import java.util.UUID;
+
+/**
+ * Dummy implementation of {@link PlayerDataSaver} that simulates a successful save.
+ * <p>
+ * This is useful for testing purposes or when no data needs to be saved.
+ * @param <T> The type of data to save
+ */
+public class DummySaver<T> implements PlayerDataSaver<T> {
+
+  @Override
+  public boolean save(UUID playerID) {
+    return true;
+  }
+}
